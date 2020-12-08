d30a1604decff6b6c64295837e7431c7fe46c08d
Carl Wilson <carl@openpreservation.org>
2020-12-08 23:05:00 +0000

    FEAT: Validator invocation - added validator CLI download and install to `.travis.yml`; - removed unnecessary echo statements from `scripts/test-current-branch.sh`; and - added single test case branch and integration branch validation invocation.

--- scripts/test-current-branch.sh
+++ scripts/test-current-branch.sh
@@ -16,26 +16,31 @@ export SCRIPT_DIR
 
 # Obtain current git branch name and take appropriate action:
 currentBranch="$(git rev-parse --abbrev-ref HEAD)"
-echo "Parsed branch:" $currentBranch
 # Pick up Travis' PR branch if it exists
-echo "Travis branch:" $TRAVIS_BRANCH
 if [[ -v TRAVIS_BRANCH ]]; then
     currentBranch=$TRAVIS_BRANCH
 fi
-echo "Post Travis branch:" $currentBranch
 # IF integration then test find and run all test cases
 if [[ "$currentBranch" == "integration" ]]; then
-  echo "Processing all test cases"
-  find ./corp* -type f -iname "testcase.xml"
+  echo $currentBranch": processing all test cases..."
   # Loop through all testCase.xml files under a filtered subset and call the test case processor
-else
-  testCase="$(echo $currentBranch | awk -F/ '{print $NF}')"
-  echo "Processing test case:" $testCase
-  find . -path \*/$testCase/\* -iname testcase.xml
+  while IFS= read -r -d '' CASE
+  do
+    ip-check -t "$CASE"
+  done <    <(find ./corp* -type f -iname "testcase.xml" -print0)
   # ELSE assume a single test case
-  # Parse test case ID from branch
-  # Locate test case file via find
-  # call test case runner on single test case file
+  else
+    # Parse test case ID from branch
+    testCase="$(echo $currentBranch | awk -F/ '{print $NF}')"
+    echo "Processing test case:" $testCase
+    # Locate test case files via find
+    while IFS= read -r -d '' CASE
+    do
+      # call test case runner on single test case file
+      ip-check -t "$CASE"
+      exitStatus=$?
+      exit $exitStatus
+    done <    <(find . -path \*/${testCase^^}/\* -iname "testcase.xml" -print0)
 fi
 # Test case processer routine:
 # IF testCase@testable == FALSE (untestable)
